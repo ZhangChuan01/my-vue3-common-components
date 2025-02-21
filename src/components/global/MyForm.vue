dca04675a7298889aeb0d3a5ffbdd725b34595d7
zhangchuan <zhangchuan@samsino.com.cn>
2025-02-21 18:29:04 +0800

    bug修复

--- src/components/global/MyForm.vue
+++ src/components/global/MyForm.vue
@@ -74,11 +74,13 @@ const deleteEmptyValue = (obj: {[key: string]: any}) => {
   })
   return obj
 }
+
 const submit = async () => {
   const valid = await checkRules()
   // console.log('valid', valid)
   if (valid) {
-    const res = deleteEmptyValue(JSON.parse(JSON.stringify(Object.assign(modelParams,formModel.value))))
+    // const res = deleteEmptyValue(JSON.parse(JSON.stringify(Object.assign(modelParams,formModel.value))))
+    const res = JSON.parse(JSON.stringify(Object.assign(modelParams,formModel.value)))
     console.log('ressssssssss', res)
     emits('submit', res)
     return res
